5fd3512229e00d200f7179b46d0511b50c717561
jiae5287 <jiae5287@gmail.com>
2022-08-02 14:28:06 +0900

    실패율 설명 수정

--- src/programmers/withExplanation/FailureRate.java
+++ src/programmers/withExplanation/FailureRate.java
@@ -6,8 +6,6 @@ import java.util.Map;
 public class FailureRate {
 
   /**
-   *            도전 중 !!
-   *
    *            << 프로그래머스 :: 실패율 >>
    *            https://school.programmers.co.kr/learn/courses/30/lessons/42889
    *
