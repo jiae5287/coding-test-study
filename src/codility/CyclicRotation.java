7980b1f10d3c6f2c7d99c75d09ced0f80ca16feb
jiae5287 <jiae5287@gmail.com>
2022-09-25 21:13:35 +0900

    CyclingRotation 풀기

---
+++ src/codility/CyclicRotation.java
@@ -0,0 +1,12 @@
+package codility;
+
+public class CyclicRotation {
+
+  public int[] solution(int[] A, int K) {
+    int[] answer = new int[A.length];
+    for (int i = 0; i < A.length; i++) {
+      answer[(i + K) % A.length] = A[i];
+    }
+    return answer;
+  }
+}
